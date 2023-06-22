8e2b049a87e6fd3d4d0463a23d0d05e6b297b851
charliel11 <chialielin@gmail.com>
2023-06-22 13:05:45 +0800

    chore(build.ps1): build project using ninja generator

--- external/build.ps1
+++ external/build.ps1
@@ -1,22 +1,45 @@
-$config = @("Debug", "Release", "RelWithDebInfo")
+if ($IsWindows) {
+    function Invoke-CmdScript {
+        param(
+            [String] $scriptName
+        )
+        $cmdLine = """$scriptName"" $args & set"
+        & $Env:SystemRoot\system32\cmd.exe /c $cmdLine |
+        select-string '^([^=]*)=(.*)$' | foreach-object {
+            $varName = $_.Matches[0].Groups[1].Value
+            $varValue = $_.Matches[0].Groups[2].Value
+            set-item Env:$varName $varValue
+        }
+    }
+    
+    Invoke-CmdScript("C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat")
+    $CC = "-DCMAKE_C_COMPILER:string=clang-cl" 
+    $CXX = "-DCMAKE_CXX_COMPILER:string=clang-cl" 
+}
 
+$d = "ninja"
+cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/Release"
+cmake --build $d/build --config Release --target install -j 20
+Remove-Item $d/build -Force -Recurse
+
+$config = @("Debug", "Release", "RelWithDebInfo")
 $d = "oneTBB"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DTBB_TEST=OFF
-    cmake --build $d/build --config $c --target install
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON -DTBB_TEST=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX 
+    cmake --build $d/build --config $c --target install -j 20
 }
-Remove-Item $d/build -Force
+Remove-Item $d/build -Force -Recurse
 
 $d = "googletest"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON
-    cmake --build $d/build --config $c --target install
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX
+    cmake --build $d/build --config $c --target install -j 20
 }
-Remove-Item $d/build -Force
+Remove-Item $d/build -Force -Recurse
 
 $d = "benchmark"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBENCHMARK_ENABLE_TESTING=OFF
-    cmake --build $d/build --config $c --target install
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBENCHMARK_ENABLE_TESTING=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX
+    cmake --build $d/build --config $c --target install -j 20
 }
-Remove-Item $d/build -Force
+Remove-Item $d/build -Force -Recurse
