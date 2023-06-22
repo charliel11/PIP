73175aa0c968c570094f22bb32c8b9e989b03c5a
charliel11 <chialielin@gmail.com>
2023-06-22 13:23:44 +0800

    chore(build.ps1): support for wsl

--- external/build.ps1
+++ external/build.ps1
@@ -14,7 +14,12 @@ if ($IsWindows) {
     
     Invoke-CmdScript("C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat")
     $CC = "-DCMAKE_C_COMPILER:string=clang-cl" 
-    $CXX = "-DCMAKE_CXX_COMPILER:string=clang-cl" 
+    $CXX = "-DCMAKE_CXX_COMPILER:string=clang-cl"
+}
+if ($IsLinux) {
+    $CC = "-DCMAKE_C_COMPILER:string=clang"
+    $CXX = "-DCMAKE_CXX_COMPILER:string=clang++"
+    $GENERATOR = "-DCMAKE_MAKE_PROGRAM:FILEPATH=/home/charlie/project/PIP/external/install/Release/bin/ninja"
 }
 
 $d = "ninja"
@@ -22,24 +27,25 @@ cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/Release"
 cmake --build $d/build --config Release --target install -j 20
 Remove-Item $d/build -Force -Recurse
 
+
 $config = @("Debug", "Release", "RelWithDebInfo")
 $d = "oneTBB"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON -DTBB_TEST=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX 
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON -DTBB_TEST=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX $GENERATOR
     cmake --build $d/build --config $c --target install -j 20
 }
 Remove-Item $d/build -Force -Recurse
 
 $d = "googletest"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBUILD_SHARED_LIBS=ON "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX $GENERATOR
     cmake --build $d/build --config $c --target install -j 20
 }
 Remove-Item $d/build -Force -Recurse
 
 $d = "benchmark"
 foreach ($c in $config) {
-    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBENCHMARK_ENABLE_TESTING=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX
+    cmake -S $d -B $d/build -DCMAKE_INSTALL_PREFIX="install/$c" -DBENCHMARK_ENABLE_TESTING=OFF "-DCMAKE_BUILD_TYPE:string=$c" -G "Ninja" $CC $CXX $GENERATOR
     cmake --build $d/build --config $c --target install -j 20
 }
 Remove-Item $d/build -Force -Recurse
