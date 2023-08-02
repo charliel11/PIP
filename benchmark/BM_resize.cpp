8df4ffb7ceb5c8d5c9f82803d1255911f9d2ad77
charliel11 <linchialey@gmail.com>
2023-08-02 17:18:25 +0800

    chore(BM_resize.cpp): memory bandwidth practices

--- benchmark/BM_resize.cpp
+++ benchmark/BM_resize.cpp
@@ -1,3 +1,4 @@
+#include "Image.h"
 #include <benchmark/benchmark.h>
 #include <resize.h>
 #include <rwimage.h>
@@ -12,10 +13,45 @@ static void BM_resize_1(benchmark::State &state) {
   ImageF32 b;
   read_image(a, (std::string(PROJECTDIR) + "/original.jpg").c_str());
   for (auto _ : state) {
-    resize(a, b, 513, 512);
+    resize(a, b, 512, 512);
   }
-  benchmark::DoNotOptimize(a);
+  benchmark::DoNotOptimize(b);
 }
-BM(resize_1);
+// BM(resize_1);
+
+constexpr size_t n = 1 << 28;
+Vector_Aligned64<float> a(n, 0);
+
+void BM_serial_add(benchmark::State &state) {
+  for (auto _ : state) {
+    for (size_t i = 0; i < n; ++i) {
+      a[i] = a[i] + 1;
+    }
+    benchmark::DoNotOptimize(a);
+  }
+}
+BM(serial_add);
+
+void BM_simd_add(benchmark::State &state) {
+  for (auto _ : state) {
+    for (size_t i = 0; i < n; i += 8) {
+      __m256 simd = _mm256_load_ps(&a[i]);
+      _mm256_stream_ps(&a[i], simd + 1);
+    }
+    benchmark::DoNotOptimize(a);
+  }
+}
+BM(simd_add);
+
+// void BM_parallel_add(benchmark::State &state) {
+//   for (auto _ : state) {
+// #pragma omp parallel for
+//     for (size_t i = 0; i < n; ++i) {
+//       a[i] = a[i] + 1;
+//     }
+//     benchmark::DoNotOptimize(a);
+//   }
+// }
+// BM(parallel_add);
 
 BENCHMARK_MAIN();
